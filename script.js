6ba87d12193e357f0b74290e8269f12e0f0edcf1
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-11-15 19:33:33 +0100

    ok

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 "use strict"
-console.log('Hello JavaScript!')
+// console.log('Hello JavaScript!')
+const validator = require('validator');
+const validateEmail = (email) => {
+return validator.isEmail(email);
+}
+console.log('Is bob@gmail.com valid email', validateEmail('bob@gmail.com'));
